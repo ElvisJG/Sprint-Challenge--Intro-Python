447677d9f036487be92073db980ca66c4336ec7c
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-02-21 20:59:48 -0500

    added CSV and working on displaying the data

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -29,7 +29,10 @@ def cityreader(cities=[]):
     # TODO Implement the functionality to read from the 'cities.csv' file
     # For each city record, create a new City instance and add it to the
     # `cities` list
-
+    with open('cities.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=",")
+        for row in spamreader:
+            cities.append(City(row[0], row[3], row[4]))
     return cities
 
 
